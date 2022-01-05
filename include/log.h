40802f08af316b2071ecff33c55cbfc5860c4138
Stenly19 <komanglangendria04@gmail.com>
2022-01-06 03:51:50 +0800

    sedikit mengubah comment pada fungsi SdlError

--- include/log.h
+++ include/log.h
@@ -16,7 +16,7 @@ namespace cookie::log
     void Info(const std::string &message);
     void Warn(const std::string &message);
     void Error(const std::string &message);
-    // Log message ditambah dengan SDL_GetError()
+    // Print message ditambah dengan SDL_GetError()
     void SdlError(const std::string &message);
 
     inline void SetLevel(Level level) { log_level = level; }
